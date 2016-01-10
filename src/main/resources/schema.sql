de37dce5f9707ad62efedc3620c1bdb5117c941c
Olle Törnström <olle@studiomediatech.com>
2016-01-10 20:28:44 +0100

    Add data source configuration
    * The most simple way in spring-boot to enable an in-memory H2
      database and provide it as a datasource to the app.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE `pet` (
+  `name` varchar(20) DEFAULT NULL,
+  `owner` varchar(20) DEFAULT NULL,
+  `species` varchar(20) DEFAULT NULL,
+  `sex` char(1) DEFAULT NULL,
+  `birth` date DEFAULT NULL,
+  `death` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
