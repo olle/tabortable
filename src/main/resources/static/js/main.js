86c8484f8534e2a8b7994d60fc8a2fb0d2c46b7d
Olle Törnström <olle@studiomediatech.com>
2016-01-24 17:05:05 +0100

    Adapt table font size based on column count

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,6 +6,24 @@
     $global.document.querySelector('body').setAttribute('class', checked ? 'night' : 'day');
   };
 
+  var _adaptTableFontSize = function () {
+    var $table = $global.document.querySelector('table');
+    var cols = $table.getAttribute('data-columns') || 0;
+
+    var colsClass = '';
+    if (cols > 22) {
+      colsClass = 'xxl';
+    } else if (cols > 14) {
+      colsClass = 'xl';
+    } else if (cols > 10) {
+      colsClass = 'l';
+    } else if (cols > 5) {
+      colsClass = 'm';
+    }
+    
+    $table.className = ($table.className + ' ' + colsClass).trim();
+  };
+
   // Public API
   $global.tabortable = {
     toggleDayNight: _toggleDayNight
@@ -19,5 +37,8 @@
   /* Triggering initial toggle on load, will reset the mode based on checkbox
      state - which is handled differently by different browsers. */
   _toggleDayNight();
+
+  /* Triggering adapt table font size on load too. */
+  _adaptTableFontSize();
 
 })(window);
